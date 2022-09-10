00fe19893faf9853b55ea9407111a0ef2fd0354c
RyanBarbalho <106884891+RyanBarbalho@users.noreply.github.com>
2022-09-10 17:59:19 -0300

    Add files via upload

---
+++ prototipo_1.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+
+int  main()
+{
+    int x;
+
+    printf("bem vindo ao gerador de avores filogenetica\ndigite o que deseja fazer:\n");
+    printf("1 - Adicionar caracteristicas\n");
+    printf("2 - Adicionar especie\n");
+    printf("3 - imprimir Arvore\n");
+
+    scanf("%d", &x);
+
+    if (x==1){
+                    
+    }
+    else if(x==2){
+
+    }
+    else if(x==3){
+
+    }
+    else return;
+
+
+
+    return 0;
+}
